7d333f449679adecb02e1c6f43530bbfb3be2c66
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-10-13 19:27:11 +0900

    Update comment in train.py

--- train.py
+++ train.py
@@ -47,8 +47,8 @@ def preprocessing_fn(raw_feature: dict):
     for (x_coords, y_coords), (width, height), (minvalid_dist, maxvalid_dist) in zip(GRIDS, OUTPUT_SHAPES, MAX_DISTS):
         x_coords = tf.reshape(x_coords, (width, height, 1))
         y_coords = tf.reshape(y_coords, (width, height, 1))
-
-        # (width, height, num_objects)
+        
+        # (width, height, num_objects + 1)
         l, r = x_coords - bbox[:, :, :, 0], bbox[:, :, :, 2] - x_coords
         t, b = y_coords - bbox[:, :, :, 1], bbox[:, :, :, 3] - y_coords
         min_dist = tf.minimum(tf.minimum(l, r), tf.minimum(t, b))
@@ -56,23 +56,23 @@ def preprocessing_fn(raw_feature: dict):
         centerness_sq = (tf.minimum(l, r) / (tf.maximum(l, r) + epsilon)
                          * tf.minimum(t, b) / (tf.maximum(t, b) + epsilon))
 
-        # (width, height, num_objects)
+        # (width, height, num_objects + 1)
         valid = minvalid_dist < max_dist
         valid = tf.logical_and(valid, max_dist <= maxvalid_dist)
         # true if the bounding box contains the pixel
         valid = tf.logical_and(valid, 0 < min_dist)
 
-        # (width, height, num_objects)
+        # (width, height, num_objects + 1)
         # Make the areas of the invalid points incredibly large
         _area = area * (tf.cast(tf.logical_not(valid),
                                 tf.float32) * 1000000 + 1)
 
         idx = tf.argmin(_area, axis=-1)
 
-        # (width, height, num_objects)
+        # (width, height, num_objects + 1)
         _class = label * tf.cast(valid, tf.int64)
 
-        # (width, height, NUM_CLASSES + 1, num_objects)
+        # (width, height, NUM_CLASSES + 1, num_objects + 1)
         _class = tf.one_hot(_class, depth=(NUM_CLASSES + 1), axis=2)
 
         # Multiply centerness_sq by valid to prevent centerness from being NaN
@@ -80,12 +80,12 @@ def preprocessing_fn(raw_feature: dict):
         centerness_sq = centerness_sq * valid
         centerness = tf.sqrt(centerness_sq)
 
-        # (width, height, 5, num_objects)
+        # (width, height, 5, num_objects + 1)
         valid = tf.reshape(valid, (width, height, 1, -1))
         centerness_and_reg = tf.stack([centerness, l, r, t, b], axis=-2)
         centerness_and_reg = centerness_and_reg * valid
 
-        # (width, height, NUM_CLASSES + 6, num_objects)
+        # (width, height, NUM_CLASSES + 6, num_objects + 1)
         output = tf.concat([_class, centerness_and_reg], axis=-2)
 
         # (width, height, NUM_CLASSES + 6)
@@ -115,8 +115,11 @@ if __name__ == "__main__":
         ds,
         epochs=90000,
         callbacks=[
-            tf.keras.callbacks.LearningRateScheduler(FCOS.lr_scheduler),
+            tf.keras.callbacks.LearningRateScheduler(
+                FCOS.make_lr_scheduler(len(ds))),
             tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1),
+            tf.keras.callbacks.CSVLogger("log.csv", append=True),
+            tf.keras.callbacks.experimental.BackupAndRestore("bck"),
         ]
     )
     m.save_weights("saved_weights")
