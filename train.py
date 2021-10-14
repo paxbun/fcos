12b51e4f92ca998847c70f4e4720f65fb13f27b5
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-10-14 13:33:11 +0900

    Make all outputs be located in out/

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import tensorflow_datasets as tfds
 import datetime
 
 if __name__ == "__main__":
-    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = "out/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     ds = tfds.load("coco", split="train", shuffle_files=True)
     ds = ds.map(
         FCOSPreprocessor(),
@@ -23,8 +23,8 @@ if __name__ == "__main__":
             tf.keras.callbacks.LearningRateScheduler(
                 FCOS.make_lr_scheduler(len(ds))),
             tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1),
-            tf.keras.callbacks.CSVLogger("log.csv", append=True),
-            tf.keras.callbacks.experimental.BackupAndRestore("bck"),
+            tf.keras.callbacks.CSVLogger("out/log.csv", append=True),
+            tf.keras.callbacks.experimental.BackupAndRestore("out/bck"),
         ]
     )
-    m.save_weights("saved_weights")
+    m.save_weights("out/saved_weights")
